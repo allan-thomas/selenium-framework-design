85a83a78079e8ae437cff1249e284177d58bc7e5
Home-PC <allanithomas@outlook.com>
2025-12-27 16:16:36 +0530

    from PC

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -5,5 +5,5 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/cucumber",glue = "javaseleniumlearning.stepDefinitions",tags = "@Regression", monochrome = true, plugin = {"html:target/cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
+// intellij push check
 }
